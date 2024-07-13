048e46c0cf35515a95c2a0306f4c80e90360a594
karinaesparza <karinaesparza296@gmail.com>
2024-07-13 17:10:26 -0300

    modal refatorado com classe

--- modules/modal.js
+++ modules/modal.js
@@ -1,21 +1,41 @@
-export default function initModal() {
-  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-  const botaoFechar = document.querySelector('[data-modal="fechar"');
-  const containerModal = document.querySelector('[data-modal="container"');
+export default class Modal {
+  constructor(botaoAbrir, botaoFechar, containerModal) {
+    this.botaoAbrir = document.querySelector(botaoAbrir);
+    this.botaoFechar = document.querySelector(botaoFechar);
+    this.containerModal = document.querySelector(containerModal);
+    /* bind this ao callback 
+    para fazer referencia ao objeto da classe
+     */
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.cliqueForaModal = this.cliqueForaModal.bind(this);
+  }
 
-  if (botaoAbrir && botaoFechar && containerModal) {
-    function toggleModal(event) {
-      event.preventDefault();
-      containerModal.classList.toggle("ativo");
+  init() {
+    if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+      this.addEventClick();
     }
+    return this;
+  }
 
-    function cliqueForaModal(event) {
-      if (event.target === this) {
-        toggleModal(event);
-      }
+  toggleModal() {
+    this.containerModal.classList.toggle("ativo");
+  }
+
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
+  }
+
+  cliqueForaModal(event) {
+    if (event.target === this.containerModal) {
+      this.toggleModal(event);
     }
-    botaoAbrir.addEventListener("click", toggleModal);
-    botaoFechar.addEventListener("click", toggleModal);
-    containerModal.addEventListener("click", cliqueForaModal);
+  }
+
+  addEventClick() {
+    this.botaoAbrir.addEventListener("click", this.eventToggleModal);
+    this.botaoFechar.addEventListener("click", this.eventToggleModal);
+    this.containerModal.addEventListener("click", this.cliqueForaModal);
   }
 }
+
--- script.js
+++ script.js
@@ -2,13 +2,13 @@ import TabNav from "./modules/initTabNav.js";
 import Accordion from "./modules/initAccordion.js";
 import ScrollSuave from "./modules/ScrollSuave.js";
 import initAnimacaoScroll from "./modules/initAnimacaoScroll.js";
-import initModal from "./modules/modal.js";
 import initToolTip from "./modules/initTooltip.js";
 import initDropdownMenu from "./modules/initDropdownMenu.js";
 import initMenuMobile from "./modules/initMenuMobile.js";
 import initFuncionamento from "./modules/initFuncionamento.js";
 import initFetchAnimais from "./modules/fetchAnimais.js";
 import initFetchBitcoin from "./modules/fetchBitcoin.js";
+import Modal from "./modules/modal.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="list"] a[href^="#"]');
 scrollSuave.init();
@@ -19,8 +19,9 @@ accordion.init();
 const tabNav = new TabNav("[data-tab='menu']", "[data-tab='content'] section");
 tabNav.init();
 
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"', '[data-modal="container"');
+modal.init();
 initAnimacaoScroll();
-initModal();
 initToolTip();
 initDropdownMenu();
 initMenuMobile();
