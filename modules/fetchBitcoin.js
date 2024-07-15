4d16b121b0e9dd1eba8b882932ffbdd2f9c00700
karinaesparza <karinaesparza296@gmail.com>
2024-07-15 20:44:32 -0300

    insere função ao escopo

--- modules/fetchBitcoin.js
+++ modules/fetchBitcoin.js
@@ -1,23 +1,24 @@
-export default function initFetchBitcoin() {}
-
-async function fetchBitcoin(url) {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    const valorDeVenda = data["BRL"]["sell"];
-    preencherElement(valorDeVenda);
-  } catch (e) {
-    throw new Error(e);
+export default function initFetchBitcoin() {
+  async function fetchBitcoin(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const valorDeVenda = data["BRL"]["sell"];
+      preencherElement(valorDeVenda);
+    } catch (e) {
+      throw new Error(e);
+    }
   }
+
+  function preencherElement(value) {
+    const span = document.querySelector(".btc-preco");
+    span.innerText = quantidadeR$EmBitcoin(value);
+  }
+
+  function quantidadeR$EmBitcoin(valueBitcoin, qtd = 100) {
+    return (qtd / valueBitcoin).toFixed(4);
+  }
+
+  fetchBitcoin("https://blockchain.info/ticker");
 }
 
-function preencherElement(value) {
-  const span = document.querySelector(".btc-preco");
-  span.innerText = quantidadeR$EmBitcoin(value);
-}
-
-function quantidadeR$EmBitcoin(valueBitcoin, qtd = 100) {
-  return (qtd / valueBitcoin).toFixed(4);
-}
-
-fetchBitcoin("https://blockchain.info/ticker");
